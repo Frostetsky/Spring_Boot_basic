2451e48b6ea314d27d6e00f204ec5b336601d6bf
Frostetskiy <nesterenko-nikita@bk.ru>
2021-06-15 04:15:15 +0300

    OneToMany City+Sight

--- src/test/java/com/nikita/spring_boot_rest_api/dao/OneToManyRelationShipTest.java
+++ src/test/java/com/nikita/spring_boot_rest_api/dao/OneToManyRelationShipTest.java
@@ -1,8 +1,14 @@
 package com.nikita.spring_boot_rest_api.dao;
 
 
+import com.nikita.spring_boot_rest_api.entity.City;
+import com.nikita.spring_boot_rest_api.entity.Sight;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +17,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class OneToManyRelationShipTest {
 
+    @Autowired
+    public CityRepository cityRepository;
+
+    @Autowired
+    public SightRepository sightRepository;
+
+    @Test
+    @Rollback
+    public void testRestApiFindAllSightInCityMoscow() {
+        City city1 = new City("Russia", "Moscow", 12_000_000L);
+        Sight sight1 = new Sight("Красная площадь");
+        Sight sight2 = new Sight("ЦДМ");
+        city1.getSights().add(sight1);
+        city1.getSights().add(sight2);
+        city1 = cityRepository.save(city1);
+        City result = cityRepository.getById(city1.getId());
+        Assert.assertEquals(result.getSights().size(), 2);
+    }
 }
